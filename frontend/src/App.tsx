03cb9ba82dda1f0ee6a7fac1473e05590949c924
charuka97 <charukatharindhu@gmail.com>
2025-02-24 11:41:59 +0530

    complete some mission functionality with Modal pop up, and remove unwanted files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useTasks } from "./hooks/useTasks";
 import CreateTask from "./components/task/CreateTask";
 import TaskList from "./components/task/TaskList";
+import Modal from "./components/ui/Modal";
 
 const App: React.FC = () => {
   const {
@@ -11,11 +12,25 @@ const App: React.FC = () => {
     handleCreateOrUpdateTask,
     handleMarkAsDone,
     handleDeleteTask,
+    modalMessage,
+    modalType,
+    setModalMessage,
+    setModalType,
   } = useTasks();
 
   return (
     <div className="bg-white min-h-screen flex items-center justify-center">
       <div className="w-full p-24">
+        {modalMessage && modalType && (
+          <Modal
+            message={modalMessage}
+            type={modalType}
+            onClose={() => {
+              setModalMessage(null);
+              setModalType(null);
+            }}
+          />
+        )}
         <div className="flex flex-col md:flex-row gap-6">
           <div className="w-full md:w-1/2 p-4">
             <h2 className="text-xl font-semibold text-gray-900 mb-4">
